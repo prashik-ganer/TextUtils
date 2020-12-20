ca6e11dca80477308dfce41d60fa8750d2ce72a0
Prashik <prashik.ganer@gmail.com>
2020-12-20 17:11:39 +0530

    Final Commit

--- textutils/settings.py
+++ textutils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e_t)mplh&350&v&21hc52s*!3035lz$pwb^tg1a-7p+*_k=glo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shielded-journey-41554.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
